9a28dd55efa8569f7950fa56303e6a7b52f3fd4c
Pyother <piotr.dominik.sobol@gmail.com>
2023-10-16 23:50:39 +0200

    Camera holding

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function start() {
     const canvas = document.getElementById("graphics");
-//Inicialize the GL contex
+    
     const gl = canvas.getContext("webgl2");
     if (gl === null) {
-    alert("Unable to initialize WebGL. Your browser or machine may not support it.");
-    return;
-}
+        alert("Unable to initialize WebGL. Your browser or machine may not support it.");
+        return;
+    }
 
-console.log("WebGL version: " + gl.getParameter(gl.VERSION));
-console.log("GLSL version: " + gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
-console.log("Vendor: " + gl.getParameter(gl.VENDOR));
+    console.log("WebGL version: " + gl.getParameter(gl.VERSION));
+    console.log("GLSL version: " + gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
+    console.log("Vendor: " + gl.getParameter(gl.VENDOR));
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-gl.viewport(0, 0, canvas.width, canvas.height);
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    gl.viewport(0, 0, canvas.width, canvas.height);
 
-const vs = gl.createShader(gl.VERTEX_SHADER);
-const fs = gl.createShader(gl.FRAGMENT_SHADER);
-const program = gl.createProgram();
+    const vs = gl.createShader(gl.VERTEX_SHADER);
+    const fs = gl.createShader(gl.FRAGMENT_SHADER);
+    const program = gl.createProgram();
 
     const vsSource =
 
@@ -33,9 +33,7 @@ const program = gl.createProgram();
             {
                 Color = color;
                 gl_Position = proj * view * model * vec4(position,1.0);
-            }`;
-
-
+        }`;
 
     const fsSource =
 
@@ -54,29 +52,24 @@ const program = gl.createProgram();
         {
 
               frag_color = vec4(Color,1.0);
-
- 
-
         }
 
         `;
 
 
-//compilation vs
-        gl.shaderSource(vs, vsSource);      
-        gl.compileShader(vs);
-        if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
-                {
-                    alert(gl.getShaderInfoLog(vs));
-                }
+    gl.shaderSource(vs, vsSource);      
+    gl.compileShader(vs);
+    if(!gl.getShaderParameter(vs, gl.COMPILE_STATUS))
+    {
+        alert(gl.getShaderInfoLog(vs));
+    }
 
-//compilation fs
-        gl.shaderSource(fs, fsSource);     
-        gl.compileShader(fs);
-        if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
-                {
-                    alert(gl.getShaderInfoLog(fs));
-                }
+    gl.shaderSource(fs, fsSource);     
+    gl.compileShader(fs);
+    if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
+    {
+        alert(gl.getShaderInfoLog(fs));
+    }
 
     if(!gl.getShaderParameter(fs, gl.COMPILE_STATUS))
     {
@@ -94,134 +87,7 @@ const program = gl.createProgram();
 
    gl.useProgram(program);
 
-const vertices =
-[
-    -0.5, -0.5, -0.5,  0.0, 0.0, 0.0,
-    0.5, -0.5, -0.5,  0.0, 0.0, 1.0,
-    0.5,  0.5, -0.5,  0.0, 1.0, 1.0,
-    0.5,  0.5, -0.5,  0.0, 1.0, 1.0,
-    -0.5,  0.5, -0.5,  0.0, 1.0, 0.0,
-    -0.5, -0.5, -0.5,  0.0, 0.0, 0.0,
-    -0.5, -0.5,  0.5,  0.0, 0.0, 0.0,
-    0.5, -0.5,  0.5,  1.0, 0.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 1.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 1.0, 1.0,
-    -0.5,  0.5,  0.5,  0.0, 1.0, 0.0,
-    -0.5, -0.5,  0.5,  0.0, 0.0, 0.0,
-    -0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    -0.5,  0.5, -0.5,  1.0, 1.0, 1.0,
-    -0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    -0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    -0.5, -0.5,  0.5,  0.0, 0.0, 0.0,
-    -0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    0.5,  0.5, -0.5,  1.0, 1.0, 1.0,
-    0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    0.5, -0.5,  0.5,  0.0, 0.0, 0.0,
-    0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    -0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    0.5, -0.5, -0.5,  1.0, 1.0, 1.0,
-    0.5, -0.5,  0.5,  1.0, 0.0, 1.0,
-    0.5, -0.5,  0.5,  1.0, 0.0, 1.0,
-    -0.5, -0.5,  0.5,  0.0, 0.0, 0.0,
-    -0.5, -0.5, -0.5,  0.0, 1.0, 0.0,
-    -0.5,  0.5, -0.5,  0.0, 1.0, 0.0,
-    0.5,  0.5, -0.5,  1.0, 1.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    0.5,  0.5,  0.5,  1.0, 0.0, 1.0,
-    -0.5,  0.5,  0.5,  0.0, 0.0, 0.0,
-    -0.5,  0.5, -0.5,  0.0, 1.0, 0.0
-];
-
-
-const buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    const position = gl.getAttribLocation(program, "position");
-    gl.enableVertexAttribArray(position);
-    gl.vertexAttribPointer(position, 3, gl.FLOAT, false, 6*4, 0);
-
-    const color = gl.getAttribLocation(program, "color");
-    gl.enableVertexAttribArray(color);
-    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 6*4, 3*4);
-
-    // macierz modelu
-    const model = mat4.create();
-    let rotate_angle = 25 * Math.PI / 180;
-    mat4.rotate(model, model, rotate_angle, [0,0,1]);
-    let uniModel = gl.getUniformLocation(program, "model");
-    gl.uniformMatrix4fv(uniModel, false, model);
-
-    // macierz projekcji
-    const proj = mat4.create();
-    mat4.perspective(proj, 60*Math.PI/180, gl.canvas.clientWidth/gl.canvas.clientHeight, 0.1, 100);
-    let uniProj = gl.getUniformLocation(program, 'proj');
-    gl.uniformMatrix4fv(uniProj, false, proj);
-
-    // macierz widoku
-    const view = mat4.create();
-    mat4.lookAt(view, [0,0,3], [0,0,-1], [0,1,0]);
-    let uniView = gl.getUniformLocation(program, 'view');
-    gl.uniformMatrix4fv(uniView, false, view);
-    let cameraPos = glm.vec3(0,0,3);
-    let cameraFront = glm.vec3(0,0,-1);
-    let cameraUp = glm.vec3(0,1,0);
-    let rotation = 0.0;
-    let cameraFront_tmp = glm.vec3(1,1,1);
-
-
-
-function draw(){
-    setCamera();
-    gl.clearColor(0, 0, 0, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.drawArrays(gl.TRIANGLES, 0, 36);
-}
-window.requestAnimationFrame(draw);
-
-// Add the event listeners for mousedown, mousemove, and mouseup
-
-var pressedKey = {};
-window.onekeyup = function (e) { pressedKey[e.keyCode] = false; }
-    window.onekeydown = function (e) { pressedKey[e.keyCode] = true; }
-
-
-    function setCamera() {
-
-        let cameraSpeed = 0.02;
-        if (pressedKey["38"]) {
-            cameraPos.x += cameraSpeed * cameraFront.x;
-            cameraPos.y += cameraSpeed * cameraFront.y;
-            cameraPos.z += cameraSpeed * cameraFront.z;
-        }
-        else if (pressedKey["37"]) {
-            rotation -= cameraSpeed;
-            cameraFront.x =  Math.sin(rotation);
-            cameraFront.z = -Math.cos(rotation);
-        }
-        else if (pressedKey["39"]) {
-            rotation += cameraSpeed;
-            cameraFront.x =  Math.sin(rotation);
-            cameraFront.z = -Math.cos(rotation);
-        }
-        else if (pressedKey["40"]) {
-            cameraPos.x -= cameraSpeed * cameraFront.x;
-            cameraPos.y -= cameraSpeed * cameraFront.y;
-            cameraPos.z -= cameraSpeed * cameraFront.z;
-        }
-        cameraFront_tmp.x = cameraPos.x+cameraFront.x;
-        cameraFront_tmp.y = cameraPos.y+cameraFront.y;
-        cameraFront_tmp.z = cameraPos.z+cameraFront.z;
-        mat4.lookAt(view, cameraPos, cameraFront_tmp, cameraUp);
-        gl.uniformMatrix4fv( uniView, false, view);
-    }
-
-
-function kostka() {
-    let punkty_ = 36;
-    var vertices = [
+    const vertices = [
         -0.5, -0.5, -0.5,  0.0, 0.0, 0.0,
         0.5, -0.5, -0.5,  0.0, 0.0, 1.0,
         0.5,  0.5, -0.5,  0.0, 1.0, 1.0,
@@ -259,42 +125,126 @@ function kostka() {
         -0.5,  0.5,  0.5,  0.0, 0.0, 0.0,
         -0.5,  0.5, -0.5,  0.0, 1.0, 0.0
     ];
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-        n_draw=punkty_;
+
+
+    const buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    const position = gl.getAttribLocation(program, "position");
+    gl.enableVertexAttribArray(position);
+    gl.vertexAttribPointer(position, 3, gl.FLOAT, false, 6*4, 0);
+
+    const color = gl.getAttribLocation(program, "color");
+    gl.enableVertexAttribArray(color);
+    gl.vertexAttribPointer(color, 3, gl.FLOAT, false, 6*4, 3*4);
+
+    // macierz modelu
+    const model = mat4.create();
+    let rotate_angle = 25 * Math.PI / 180;
+    mat4.rotate(model, model, rotate_angle, [0,0,1]);
+    let uniModel = gl.getUniformLocation(program, "model");
+    gl.uniformMatrix4fv(uniModel, false, model);
+
+    // macierz projekcji
+    const proj = mat4.create();
+    mat4.perspective(proj, 60*Math.PI/180, gl.canvas.clientWidth/gl.canvas.clientHeight, 0.1, 100);
+    let uniProj = gl.getUniformLocation(program, 'proj');
+    gl.uniformMatrix4fv(uniProj, false, proj);
+
+    // macierz widoku
+    const view = mat4.create();
+    mat4.lookAt(view, [0,0,3], [0,0,-1], [0,1,0]);
+    let uniView = gl.getUniformLocation(program, 'view');
+    gl.uniformMatrix4fv(uniView, false, view);
+    let cameraPos = glm.vec3(0,0,3);
+    let cameraFront = glm.vec3(0,0,-1);
+    let cameraUp = glm.vec3(0,1,0);
+    let rotation = 0.0;
+    let cameraFront_tmp = glm.vec3(1,1,1);
+
+    var pressedKey = {};
+    window.onekeyup = function (e) { pressedKey[e.keyCode] = false; }
+    window.onekeydown = function (e) { pressedKey[e.keyCode] = true; }
+
+    function setCamera() {
+        let cameraSpeed = 0.02;
+        if (pressedKey["38"]) {
+            cameraPos.x += cameraSpeed * cameraFront.x;
+            cameraPos.y += cameraSpeed * cameraFront.y;
+            cameraPos.z += cameraSpeed * cameraFront.z;
+        }
+        else if (pressedKey["37"]) {
+            rotation -= cameraSpeed;
+            cameraFront.x =  Math.sin(rotation);
+            cameraFront.z = -Math.cos(rotation);
+        }
+        else if (pressedKey["39"]) {
+            rotation += cameraSpeed;
+            cameraFront.x =  Math.sin(rotation);
+            cameraFront.z = -Math.cos(rotation);
+        }
+        else if (pressedKey["40"]) {
+            cameraPos.x -= cameraSpeed * cameraFront.x;
+            cameraPos.y -= cameraSpeed * cameraFront.y;
+            cameraPos.z -= cameraSpeed * cameraFront.z;
+        }
+        cameraFront_tmp.x = cameraPos.x+cameraFront.x;
+        cameraFront_tmp.y = cameraPos.y+cameraFront.y;
+        cameraFront_tmp.z = cameraPos.z+cameraFront.z;
+        mat4.lookAt(view, cameraPos, cameraFront_tmp, cameraUp);
+        gl.uniformMatrix4fv( uniView, false, view);
     }
+
+    var pressedKey = {};
+
+    window.addEventListener("keydown", function (e) {
+        pressedKey[e.keyCode] = true;
+    });
+
+    window.addEventListener("keyup", function (e) {
+        pressedKey[e.keyCode] = false;
+    });
+
+    function draw() {
+        setCamera();
+        gl.clearColor(0, 0, 0, 1);
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.drawArrays(gl.TRIANGLES, 0, 36);
+        window.requestAnimationFrame(draw);
+    }
+    window.requestAnimationFrame(draw);
 }
 
 
 const alert = document.getElementById("alert");
 
-    // Add the event listeners for mousedown, mousemove, and mouseup
-    window.addEventListener('mousedown', e => {
-        x = e.offsetX;
-        y = e.offsetY;
-        const paragraph = document.createElement("p");
-        const text = document.createTextNode(`X : ${x}, Y : ${y}`);
-        paragraph.appendChild(text);
-        alert.appendChild(paragraph);
-    });
+window.addEventListener('mousedown', e => {
+    x = e.offsetX;
+    y = e.offsetY;
+    const paragraph = document.createElement("p");
+    const text = document.createTextNode(`X : ${x}, Y : ${y}`);
+    paragraph.appendChild(text);
+    alert.appendChild(paragraph);
+});
 
-// Add the event listeners for keydown, keyup
 window.addEventListener('keydown', function(event) {
     const paragraph = document.createElement("p");
     let text;
     switch (event.keyCode) {
-        case 37: // Left
+        case 37: 
             text = document.createTextNode("Lewo");
         break;
 
-        case 38: // Up
+        case 38: 
             text = document.createTextNode("Góra");
         break;
 
-        case 39: // Right
+        case 39: 
             text = document.createTextNode("Prawo");
         break;
 
-        case 40: // Down
+        case 40: 
             text = document.createTextNode("Dół");
         break;
     }
